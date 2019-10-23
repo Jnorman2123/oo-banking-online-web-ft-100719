35248547db553309a332fab62a0d72f5c5518400
Jonathan Norman <jnorman2123@gmail.com>
2019-10-23 17:30:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif
+    elsif @sender.balance < self.amount
       p "Transaction rejected. Please check your account balance."
     end
   end
