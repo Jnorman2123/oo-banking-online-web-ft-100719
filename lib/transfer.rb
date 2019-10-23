1e792ecd3ed77d51ae926fe8c3d119301447ef67
Jonathan Norman <jnorman2123@gmail.com>
2019-10-23 17:27:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,10 +13,11 @@ class Transfer
   end
 
   def execute_transaction
-    if status == "pending"
+    if status == "pending" && @sender.balance > self.amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+    else
     end
   end
 end
