b0ee2fbe48700453e9803f5bc0b507cf1e630b4b
Jonathan Norman <jnorman2123@gmail.com>
2019-10-23 17:23:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else 
+    else
       p "Transaction already completed."
     end
   end
