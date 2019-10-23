c021c3f6fb23ffeb90e529f946233e2142f96e9d
Jonathan Norman <jnorman2123@gmail.com>
2019-10-23 17:30:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,11 +13,11 @@ class Transfer
   end
 
   def execute_transaction
-    if status == "pending"
+    if status == "pending" && @sender.balance > self.amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif @sender.balance < self.amount
+    elsif
       p "Transaction rejected. Please check your account balance."
     end
   end
